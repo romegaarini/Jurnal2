42fa9b893676d88c8b6e888cec8c3938eeb943d4
romegaarini <71334324+romegaarini@users.noreply.github.com>
2020-09-19 22:50:07 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,7 @@
+public class tes {
+    public static void main(String[]args){
+        System.out.println("Nama : Romega Arini\n");
+        System.out.println("NIM : 6706190031\n");
+        System.out.println("Kelas : 43-03\n");
+    }
+}
